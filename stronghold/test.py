ca28e0bcbcceebea15700f637d9d983dd350fa69
wendy <jwkwendy@gmail.com>
2025-05-09 22:46:40 -0700

    Update test.py

--- stronghold/test.py
+++ stronghold/test.py
@@ -82,3 +82,90 @@ Dna = [
     "CAATAATATTCG"
 ]
 print(GreedyMotifSearch(Dna, k, t))
+
+# Input:  A list of kmers Dna, and integers k and t (where t is the number of kmers in Dna)
+# Output: GreedyMotifSearch(Dna, k, t)
+def CountWithPseudocounts(Motifs):
+    count = {symbol: [1] * len(Motifs[0]) for symbol in "ACGT"}
+    for i in range(len(Motifs)):
+        for j in range(len(Motifs[i])):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+
+def ProfileWithPseudocounts(Motifs):
+    t = len(Motifs) + 4
+    count = CountWithPseudocounts(Motifs)
+    profile = {'A': [], 'C': [], 'G': [], 'T': []}
+    for i in range(len(Motifs[0])):
+        for nucleotide in ['A', 'C', 'G', 'T']:
+            profile[nucleotide].append(count[nucleotide][i] / t)
+    return profile
+
+def ScoreWithPseudocounts(Motifs):
+    consensus = ConsensusWithPseudocounts(Motifs)
+    k = len(consensus)
+    t = len(Motifs)
+    score = 0
+    for j in range(k):
+        for i in range(t):
+            if Motifs[i][j] != consensus[j]:
+                score += 1
+    return score
+
+def ConsensusWithPseudocounts(Motifs):
+    profile = ProfileWithPseudocounts(Motifs)
+    consensus = ""
+    k = len(Motifs[0])
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if profile[symbol][j] > m:
+                m = profile[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
+
+def ProfileMostProbablePattern(Text, k, profile):
+    max_prob = -1
+    most_probable = ""
+    for i in range(len(Text) - k + 1):
+        pattern = Text[i:i+k]
+        prob = 1
+        for j in range(k):
+            prob *= profile[pattern[j]][j]
+        if prob > max_prob:
+            max_prob = prob
+            most_probable = pattern
+    return most_probable
+
+def GreedyMotifSearchWithPseudocounts(Dna, k, t):
+    BestMotifs = [string[:k] for string in Dna]
+    
+    for i in range(len(Dna[0]) - k + 1):
+        Motifs = [Dna[0][i:i+k]]
+        
+        for j in range(1, t):
+            profile = ProfileWithPseudocounts(Motifs)
+            motif = ProfileMostProbablePattern(Dna[j], k, profile)
+            Motifs.append(motif)
+        
+        if ScoreWithPseudocounts(Motifs) < ScoreWithPseudocounts(BestMotifs):
+            BestMotifs = Motifs
+    
+    return BestMotifs
+# Sample Input:
+# 3 5
+# GGCGTTCAGGCA
+# AAGAATCAGTCA
+# CAAGGAGTTCGC
+# CACGTCAATCAC
+# CAATAATATTCG
+# Sample Output:
+# TTC
+# ATC
+# TTC
+# ATC
+# TTC
+#######################################################
