d3ce2d3b5be7b6e192a15fe7676e9d805cf30481
wendy <jwkwendy@gmail.com>
2025-06-25 23:16:03 -0700

    boyerMoore.py

---
+++ bioinformatic/BoyerMoore.py
@@ -0,0 +1,85 @@
+def boyer_moore(p, p_bm, t):
+    """ Do Boyer-Moore matching """
+    i = 0
+    occurrences = []
+    while i < len(t) - len(p) + 1:
+        shift = 1
+        mismatched = False
+        for j in range(len(p)-1, -1, -1):
+            if p[j] != t[i+j]:
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
+                mismatched = True
+                break
+        if not mismatched:
+            occurrences.append(i)
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
+        i += shift
+    return occurrences
+
+t = 'GCTAGCTCTACGAGTCTA'
+p = 'TCTA'
+p_bm = BoyerMoore(p, alphabet='ACGT')
+print(boyer_moore(p, p_bm, t))
+#-------------------------------------
+
+# GCTAGCTCTACGAGTCTA
+p = 'TCAA'
+p_bm = BoyerMoore(p, alphabet='ACGT')
+p_bm.bad_character_rule(2, 'T')
+#-------------------------------------
+
+# GCTAGCTCTACGAGTCTA
+# ACTA
+p = 'ACTA'
+p_bm = BoyerMoore(p, alphabet='ACGT')
+p_bm.good_suffix_rule(0)
+#-------------------------------------
+
+# ACACGCTCTACGAGTCTA
+# ACAC
+p = 'ACAC'
+p_bm = BoyerMoore(p, alphabet='ACGT')
+p_bm.match_skip()
+
+#-------------------------------------
+
+class BoyerMoore(object):
+    """ Encapsulates pattern and associated Boyer-Moore preprocessing. """
+    
+    def __init__(self, p, alphabet='ACGT'):
+        self.p = p
+        self.alphabet = alphabet
+        # Create map from alphabet characters to integers
+        self.amap = {}
+        for i in range(len(self.alphabet)):
+            self.amap[self.alphabet[i]] = i
+        # Make bad character rule table
+        self.bad_char = dense_bad_char_tab(p, self.amap)
+        # Create good suffix rule table
+        _, self.big_l, self.small_l_prime = good_suffix_table(p)
+    
+    def bad_character_rule(self, i, c):
+        """ Return # skips given by bad character rule at offset i """
+        assert c in self.amap
+        ci = self.amap[c]
+        assert i > (self.bad_char[i][ci]-1)
+        return i - (self.bad_char[i][ci]-1)
+    
+    def good_suffix_rule(self, i):
+        """ Given a mismatch at offset i, return amount to shift
+            as determined by (weak) good suffix rule. """
+        length = len(self.big_l)
+        assert i < length
+        if i == length - 1:
+            return 0
+        i += 1  # i points to leftmost matching position of P
+        if self.big_l[i] > 0:
+            return length - self.big_l[i]
+        return length - self.small_l_prime[i]
+    
+    def match_skip(self):
+        """ Return amount to shift in case where P matches T """
+        return len(self.small_l_prime) - self.small_l_prime[1]
